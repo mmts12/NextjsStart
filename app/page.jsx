e42308b6d9aeff4ef43a61ad7d5572f716814d6c
mmts12 <mmts12@gmail.com>
2024-04-12 10:37:54 +0300

    add few component

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import Feed from '@components/Feed'
+
 const Home = () => {
     return (
         <section className="w-full flex-center flex-col">
@@ -5,10 +7,12 @@ const Home = () => {
             <br className="max-md:hidden"/>
             <span className="orange_gradient text-center">AI Powerd Prompts</span>
             </h1>
-            <p>
+            <p className="desc text-center">
                 promptopia is an open-source AI prompting tool for 
                 modern world to discover ,create and share creative prompts
             </p>
+
+            <Feed/>
         </section>
     )
 }
