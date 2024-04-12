4f5adc8cb7d57042f08acb887cd2ab02f7168fd7
mmts12 <mmts12@gmail.com>
2024-04-12 09:44:20 +0300

    first nextjs

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "promptopia",
+    description: 'Discover &Share AI Prompts'
+}
+
+const RootLayout = ({children}) => {
+    return (
+        <html>
+            <body>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
+                <main className='app'>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
+}
+
+export default RootLayout;
